e956f72e4f0aa8bbf1ae20645f1fa0e3a2b7ab7f
xiashilin <1479714932@qq.com>
2016-11-30 16:47:40 +0800

    提交

---
+++ app/src/main/java/ydkf/niit/cn/ui/utils/StatusBarView.java
@@ -0,0 +1,18 @@
+package ydkf.niit.cn.ui.utils;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by xsl on 2016/9/30.
+ */
+public class StatusBarView extends View {
+    public StatusBarView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public StatusBarView(Context context) {
+        super(context);
+    }
+}
